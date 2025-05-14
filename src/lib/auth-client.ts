66eb124157165a0e4aadc3a5620fa747c18904d8
Ramis Samadov <one.sramis@gmail.com>
2025-05-14 12:19:22 +0400

    refactor(auth): update auth-client to use baseURL from env variables

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,5 +1,6 @@
+import { PUBLIC_APP_URL } from "$env/static/public";
 import { createAuthClient } from "better-auth/svelte";
 
 export const authClient = createAuthClient({
-  baseURL: "http://localhost:5173",
+  baseURL: PUBLIC_APP_URL,
 });
